84e54cb9d42f8da73488ffd2f60d662b6053c562
Arnaud Mengus <44212923+isontheline@users.noreply.github.com>
2024-07-09 21:11:09 +0200

    Fixing invalid entity identifier

--- raprnsa/CHANGELOG.md
+++ raprnsa/CHANGELOG.md
@@ -1,3 +1,4 @@
-| Version | Change|
-| --- | --- |
-| **0.1** | initial release|
+| Version | Change | Date |
+| --- | --- | --- |
+| **0.2** | Fixing invalid entity identifier | 2024-07-09 |
+| **0.1** | Initial Release | 2024-07-09 |
--- raprnsa/run.sh
+++ raprnsa/run.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/with-contenv bashio
 COUNTY=$(cat options.json |jq -r '.County')
 until false; do 
-  curl "https://pollens.fr/load_vigilance_map" -o /data/rnsa_load_vigilance_map.json
+  curl -s -X GET "https://pollens.fr/load_vigilance_map" -o /data/rnsa_load_vigilance_map.json
   RISK_LEVEL=$(cat /data/rnsa_load_vigilance_map.json | jq '.vigilanceMapCounties | fromjson | .["'$COUNTY'"].riskLevel')
   echo "Current Risk Level for County '$COUNTY' : $RISK_LEVEL"
-  curl -s -X POST -H "Content-Type: application/json"  -H "Authorization: Bearer $HASSIO_TOKEN" -d '{"state": "'$RISK_LEVEL'", "attributes":  {"unit_of_measurement": "state", "icon": "mdi:flower-pollen", "friendly_name": "Risk Level"}}' http://hassio/homeassistant/api/states/sensor.raprnsa.county_$COUNTY 2>/dev/null
+  curl -s -X POST -H "Content-Type: application/json"  -H "Authorization: Bearer $HASSIO_TOKEN" -d '{"state": "'$RISK_LEVEL'", "attributes":  {"unit_of_measurement": "state", "icon": "mdi:flower-pollen", "friendly_name": "Risk Level"}}' http://hassio/homeassistant/api/states/weather.raprnsa_county_$COUNTY 2>/dev/null
   sleep 120;
 done
